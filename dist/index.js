732bf446adda38985a667b3ac1511643160e91c6
Shadow如羽 <xuting90@gmail.com>
2018-11-30 07:41:27 +0800

    将所有文件放到index.ts中导出

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,17 @@
 export * from './wy-components.module';
 export * from "./safe-res";
+//
+export * from "./wy-image-viewer/wy-image-viewer";
+export * from "./wy-image-viewer/wy-image-viewer-closer.gesture";
+export * from "./wy-image-viewer/wy-image-viewer-config";
+export * from "./wy-image-viewer/wy-image-viewer-impl";
+export * from "./wy-image-viewer/wy-image-viewer-transitions";
+export * from "./wy-image-viewer/wy-image-viewer.component";
+export * from "./wy-image-viewer/wy-image-viewer.controller";
+export * from "./wy-image-viewer/wy-image-viewer.directive";
+//
+export * from "./wy-slides/wy-slide-zoom.gesture";
+export * from "./wy-slides/wy-slides";
+export * from "./wy-slides/wy-slides.gesture";
+export * from "./wy-slides/wy-slides.manage";
+export * from "./wy-slides/wy-slide";
