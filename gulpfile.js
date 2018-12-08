ffb563e7400b4be1578acdad7cac39e532731ec7
Shadow如羽 <xuting90@gmail.com>
2018-12-08 14:46:16 +0800

    自动编译

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
 const gulp = require("gulp");
-gulp.task('default', () => {
+const shell = require("gulp-shell");
+const del = require("del");
+
+gulp.task('move-scss', ["clean"], () => {
     gulp.src('components/wy-image-viewer/wy-image-viewer.scss')
         .pipe(gulp.dest("bin/components/wy-image-viewer"));
     gulp.src("components/wy-slides/wy-slides.scss")
         .pipe(gulp.dest("bin/components/wy-slides"));
 });
+
+gulp.task('build', ["move-scss"], () => {
+    console.log('build');
+    return shell.task('nodule_modules/.bin/tsc')
+});
+gulp.task('clean', [], (cb) => {
+    return del([
+        "./bin"
+    ], cb)
+});
+gulp.task('default', ["move-scss"]);
